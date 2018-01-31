b084bf21bd7cce56cf0f28cd35a2abd865cdf3bf
Kevin <kmcbrayer77@gmail.com>
2018-01-31 16:51:19 -0500

    use correct pathing for containers

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,16 +14,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import HomePage from '../HomePage/Loadable';
+import NotFoundPage from '../NotFoundPage/Loadable';
 
 export default function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    );
 }
