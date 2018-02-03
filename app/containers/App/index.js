74ffedee250a8ab8341d37e92923bf02db0de37d
Kevin <kmcbrayer77@gmail.com>
2018-02-02 20:55:48 -0500

    add a login page with and redirect to it if the user is not logged in
    created a userReducer with default state for now

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,36 +1,37 @@
-/**
- *
- * App.js
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import HomePage from '../HomePage/Loadable';
+import LoginPage from '../Login/Loadable';
 import NotFoundPage from '../NotFoundPage/Loadable';
 
-export default function App() {
-    const loggedIn = store.user.id; // isLoggedIn()?
-    return (
-        <div>
-            <Switch>
-                <Route exact path="/" render={() => (
-                    loggedIn ? (
-                        <Redirect to="/login" />
-                    ) : (
-                        <HomePage />
-                    )
-                )} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    );
+class App extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path="/" render={() => (
+                        this.props.isLoggedIn ? (
+                            <HomePage />
+                        ) : (
+                            <LoginPage />
+                        )
+                    )} />
+                    <Route path="/login" component={LoginPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </div>
+        );
+    }
 }
+
+const mapStateToProps = (state) => ({
+    isLoggedIn: state.user.isLoggedIn
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    // not used yet
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
