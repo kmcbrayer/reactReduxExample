6b73d0810bf9f6174287409216f5deb3d0ba7bf9
Kevin <kmcbrayer77@gmail.com>
2018-02-10 00:09:06 -0500

    remove route reducer

--- app/redux/reducers.js
+++ app/redux/reducers.js
@@ -1,41 +1,11 @@
 import { combineReducers } from 'redux';
-import { LOCATION_CHANGE } from 'react-router-redux';
 import userReducer from './User/UserReducer';
-
-/*
- * routeReducer
- *
- * The reducer merges route location changes into our immutable state.
- * The change is necessitated by moving to react-router-redux@4
- *
- */
-
-// Initial routing state
-const routeInitialState = {
-    location: null,
-};
-
-/**
- * Merge route into the global application state
- */
-function routeReducer(state = routeInitialState, action) {
-    switch (action.type) {
-        /* istanbul ignore next */
-        case LOCATION_CHANGE:
-            return {
-                location: action.payload,
-            };
-        default:
-            return state;
-    }
-}
 
 /**
  * Creates the main reducer with the dynamically injected ones
  */
 export default function createReducer(injectedReducers) {
     return combineReducers({
-        route: routeReducer,
         user: userReducer,
         ...injectedReducers,
     });
