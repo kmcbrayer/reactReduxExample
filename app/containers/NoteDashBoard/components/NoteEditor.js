8a56cac5eb076cb157aff87609db632f76a75e8d
Kevin <kmcbrayer77@gmail.com>
2018-02-21 07:19:49 -0500

    update NoteEditor to handle title/body changes

--- app/containers/NoteDashBoard/components/NoteEditor.js
+++ app/containers/NoteDashBoard/components/NoteEditor.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NoteEditor = ({ note }) => (
-    <div>
-        { note ? (
+class NoteEditor extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = this.props.note;
+    }
+
+    titleChangeKeyUpHandler = (e) => {
+        e.preventDefault();
+        const title = e.target.value;
+        this.setState({ title });
+        this.props.noteChangeHandler(this.state);
+    };
+
+    bodyChangeKeyUpHandler = (e) => {
+        e.preventDefault();
+        const body = e.target.value;
+        this.setState({ body });
+        this.props.noteChangeHandler(this.state);
+    };
+
+    render() {
+        return (
             <div>
-                <h1>{note.title}</h1>
-                <p>{note.body}</p>
+                { this.state.id ? (
+                    <div>
+                        <input
+                            type="text"
+                            value={this.state.title}
+                            onChange={this.titleChangeKeyUpHandler}
+                            onKeyUp={this.titleChangeKeyUpHandler} />
+                        <textarea
+                            value={this.state.body}
+                            onChange={this.bodyChangeKeyUpHandler}
+                            onKeyUp={this.bodyChangeKeyUpHandler} />
+                    </div>
+                ) : null }
             </div>
-        ) : null }
-    </div>
-);
+        );
+    }
+}
+
 
 NoteEditor.propTypes = {
     note: PropTypes.object,
+    noteChangeHandler: PropTypes.func
 };
 
 export default NoteEditor;
