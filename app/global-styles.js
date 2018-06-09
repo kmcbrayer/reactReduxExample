131673e04c5d2d7415478bcd42e8d81788f2d89e
Kevin <kmcbrayer77@gmail.com>
2018-06-09 09:06:48 -0400

    add Open Sans font

--- app/global-styles.js
+++ app/global-styles.js
@@ -2,30 +2,44 @@ import { injectGlobal } from 'styled-components';
 
 /* eslint no-unused-expressions: 0 */
 injectGlobal`
-  html,
-  body {
-    padding:0;
-    margin:0;
-    height: 100%;
-    width: 100%;
-  }
+    html, body {
+        padding:0;
+        margin:0;
+        height: 100%;
+        width: 100%;
+    }
 
-  body {
-    font-family: gotham, helvetica, arial, sans-serif;
-  }
-
-  body.fontLoaded {
-    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  }
-
-  #app {
-    background-color: #fafafa;
-    min-height: 100%;
-    min-width: 100%;
-  }
-
-  p,
-  label {
-    line-height: 1.5em;
-  }
+    body {
+        font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+    }
+    
+    h1, h2, h3 {
+        font-family: 'Open Sans Bold', Helvetica, Arial, sans-serif;
+    }
+    
+    @font-face {
+        font-family: 'Open Sans';
+        src: url('OpenSans-Regular.ttf');
+        src: url('OpenSans-Regular.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+    }
+    
+    @font-face {
+        font-family: 'Open Sans Bold';
+        src: url('OpenSans-Bold.ttf');
+        src: url('OpenSans-Bold.ttf') format('truetype');
+        font-weight: bold;
+        font-style: bold;
+    }
+    
+    #app {
+        background-color: #fafafa;
+        min-height: 100%;
+        min-width: 100%;
+    }
+    
+    p, label {
+        line-height: 1.5em;
+    }
 `;
