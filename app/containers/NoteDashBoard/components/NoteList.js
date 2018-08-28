c9e1d1096b013d80ccf98c321658a1c8755f0e9e
Kevin <kmcbrayer77@gmail.com>
2018-08-28 15:34:41 -0400

    migrate styles to components

--- app/containers/NoteDashBoard/components/NoteList.js
+++ app/containers/NoteDashBoard/components/NoteList.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
 import NoteListItem from './NoteListItem';
+
+const NoteListContainer = styled.div`
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    width: 30vw;
+    border-right: 1px solid #90efdf;
+    float:left
+`;
 
 const NoteList = ({ notes, noteClick }) => {
     const noteItems = notes.sort((a, b) => b.lastUpdated - a.lastUpdated).map((note) => (
@@ -8,9 +19,9 @@ const NoteList = ({ notes, noteClick }) => {
     ));
 
     return (
-        <div>
+        <NoteListContainer>
             {noteItems}
-        </div>
+        </NoteListContainer>
     );
 };
 
